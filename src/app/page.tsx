b4426eef0ee56a8e3a2892f0e083f0f6f6767085
Chinmay <72600485+ChinmayOnGithub@users.noreply.github.com>
2025-06-06 23:57:38 +0530

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen bg-gray-600 text-gray-50 selection:bg-gray-100 select-none selection:text-gray-800">
+			<h1 className="text-7xl font-bold">Portfolio</h1>
+			<p className="text-3xl text-gray-400">Welcome to my portfolio website!</p>
+		</div>
+	);
+}
